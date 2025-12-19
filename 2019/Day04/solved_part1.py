7f9dea406e40b52d011f7a0bda31734cf8582ce3
Alejandro Lopez Monzon <alexlm78@gmail.com>
2025-12-18 20:49:23 -0600

    Advent of Code 2019 - Day 04

---
+++ 2019/Day04/solved_part1.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+with open('input.txt', 'r') as file:
+  data = file.read().splitlines()
+
+def solve(input):
+  lower, upper = map(int, input[0].split("-"))
+  n = 0
+
+  for x in range(lower, upper):
+    candidate = str(x)
+    counts = Counter(candidate)
+    twosame = max(counts.values()) >= 2
+    increases = list(candidate) == sorted(candidate)
+    if twosame and increases: n += 1
+
+  return n
+
+print(solve(data))
